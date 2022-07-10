4e1b3965e6c17257ee1798b8c4554ad4058e3b14
miguel castro <miguel.castro9@hotmail.com>
2022-07-10 14:13:44 -0300

    api-web add service

---
+++ api-web/src/app/model/pessoa.ts
@@ -0,0 +1,8 @@
+export class Pessoa {
+
+  id!: number;
+  nome!: string;
+  email!: string;
+
+  constructor(){}
+}
---
+++ api-web/src/app/service/pessoas.service.ts
@@ -0,0 +1,40 @@
+import { Pessoa } from './../model/pessoa';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoasService {
+
+  pessoa: Pessoa[] = [];
+  url = 'http://localhost:8080/pessoas';
+
+  constructor(private http: HttpClient) { }
+
+  listaPessoa(){
+
+    return this.http.get<Pessoa[]>(this.url + '/lista');
+  }
+
+  salvaPessoa(pessoa: Pessoa){
+
+    return this.http.post<Pessoa>(this.url + '/salva', pessoa);
+  }
+
+  alteraPessoa(pessoa: Pessoa){
+
+    return this.http.put<Pessoa>(this.url + '/altera/', pessoa.id);
+  }
+
+  buscaPessoa(id: any){
+
+    return this.http.get<Pessoa>(this.url + '/busca/' + id);
+
+  }
+
+  deletaPessoa(pessoa: Pessoa){
+
+    return this.http.delete<Pessoa>(this.url + '/deleta/' + pessoa.id);
+  }
+}
